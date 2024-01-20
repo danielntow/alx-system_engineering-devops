111a2837b9382f6879b7472a5acda4ab4b38d5c7
danielntow <danielntow@gmail.com>
2024-01-20 13:43:39 +0000

    configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,9 +3,9 @@
 # Explanation: Create a file in /tmp named 'school' with the given permissions, owner, group, and content.
 
 file { '/tmp/school':
-  ensure   => file,
-  mode     => '0744',
-  owner    => 'www-data',
-  group    => 'www-data',
-  content  => 'I love Puppet',
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
