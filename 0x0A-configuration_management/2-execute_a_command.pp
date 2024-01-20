40b43c61e1a7ebab7c2a3730ddbb0315bb12998c
danielntow <danielntow@gmail.com>
2024-01-20 14:28:14 +0000

    configuration management- install flask

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,4 @@
-# 2-execute_a_command.pp
-
-# Explanation: Kill the process named "killmenow" using pkill.
-
+# kill a process
 exec { 'killmenow':
-  command => 'pkill -f killmenow',
-  onlyif  => 'pgrep -f killmenow',
+  command => '/bin/pkill killmenow',
 }
