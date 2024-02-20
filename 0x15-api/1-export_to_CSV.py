207b683f5f10e73c8faee30279b0422197a6d6ea
danielntow <danielntow@gmail.com>
2024-02-20 04:18:05 +0000

    export to .csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,40 +1,22 @@
 #!/usr/bin/python3
-"""
-This script retrieves and exports information about an employee's
-n TODO list progress in CSV format using a REST API.
-"""
-
+"""gather data from an API"""
 import csv
-from sys import argv
+import json
 import requests
+from sys import argv
 
+emp_info = 'https://jsonplaceholder.typicode.com/users/'
+all_todos = 'https://jsonplaceholder.typicode.com/todos'
 if __name__ == "__main__":
-    # if len(argv) != 2 or not argv[1].isdigit():
-    #     print("Usage: {} <employee_id>".format(argv[0]))
-    #     exit(1)
+    emp_id = argv[1]
+    get_emp_info = json.loads(requests.get(emp_info + emp_id).text)
+    get_all_todos = json.loads(requests.get(all_todos).text)
+    todos_done_list = []
+    name = None
 
-    employee_id = int(argv[1])
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
-        employee_id)
-    tasks_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-        employee_id)
-
-    try:
-        user_response = requests.get(user_url).json()
-        tasks_response = requests.get(tasks_url).json()
-
-        employee_name = user_response.get('username')
-        csv_filename = "{}.csv".format(employee_id)
-
-        with open(csv_filename, mode='w', newline='') as csvfile:
-            csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-            csv_writer.writerow(
-                ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
-            for task in tasks_response:
-                csv_writer.writerow(
-                    [employee_id, employee_name, str(
-                        task['completed']), task['title']])
-
-    except requests.exceptions.RequestException as e:
-        print("Error:", e)
+    with open('{}.csv'.format(emp_id), 'w', encoding="utf8") as f:
+        for i in get_all_todos:
+            if i['userId'] == int(emp_id):
+                f.write('"{}","{}","{}","{}"\n'.format(
+                    emp_id, get_emp_info['username'],
+                    i['completed'], i['title']))
