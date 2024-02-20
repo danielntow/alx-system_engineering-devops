aa19ed05a502056a99c86f4fe4888b0c7734c577
danielntow <danielntow@gmail.com>
2024-02-20 04:21:18 +0000

    export to .csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
-"""
-Gather data from an API and export to CSV
-"""
-
+"""gather data from an API"""
 import csv
 import json
 import requests
 from sys import argv
 
-EMP_INFO_URL = 'https://jsonplaceholder.typicode.com/users/'
-ALL_TODOS_URL = 'https://jsonplaceholder.typicode.com/todos'
-
+emp_info = 'https://jsonplaceholder.typicode.com/users/'
+all_todos = 'https://jsonplaceholder.typicode.com/todos'
 if __name__ == "__main__":
-    if len(argv) != 2 or not argv[1].isdigit():
-        print("Usage: {} <employee_id>".format(argv[0]))
-        exit(1)
-
     emp_id = argv[1]
-    emp_info_response = requests.get(EMP_INFO_URL + emp_id).json()
-    all_todos_response = requests.get(ALL_TODOS_URL).json()
-
+    get_emp_info = requests.get(emp_info + emp_id).json()
+    get_all_todos = requests.get(all_todos).json()
     todos_done_list = []
-    with open('{}.csv'.format(emp_id), 'w', encoding="utf8", newline='') as csvfile:
-        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        csv_writer.writerow(
-            ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+    name = None
 
-        for todo in all_todos_response:
-            if todo['userId'] == int(emp_id):
-                csv_writer.writerow(
-                    [emp_id, emp_info_response['username'], str(todo['completed']), todo['title']])
-
-    print("CSV file '{}.csv' has been created.".format(emp_id))
+    with open('{}.csv'.format(emp_id), 'w', encoding="utf8") as f:
+        for i in get_all_todos:
+            if i['userId'] == int(emp_id):
+                f.write('"{}","{}","{}","{}"\n'.format(
+                    emp_id, get_emp_info['username'],
+                    i['completed'], i['title']))
