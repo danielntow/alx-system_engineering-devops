e5c49fda85e4fe5e944b43fa6a5e999c3ae92fb4
danielntow <danielntow@gmail.com>
2024-03-06 05:46:47 +0000

    correct pep8

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -57,6 +57,7 @@ def recurse(subreddit, hot_list=[], after=None):
         print(f"Error: {response.status_code}")
         return None
 
+
 if __name__ == "__main__":
     # Test the function with command-line arguments
     import sys
